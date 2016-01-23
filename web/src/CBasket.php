915e2dd40734abcd8f2930d1857f4d1230f81fee
ArcusVII <lukasz.przepiorka93@gmail.com>
2016-01-23 13:22:24 +0100

    Aktualizacja klas

---
+++ web/src/CBasket.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @access public
+ * @author £uki
+ */
+class CBasket {
+	private $m_ID;
+	private $m_IDUzytkownika;
+	private $m_ListaProduktow;
+
+	/**
+	 * @access public
+	 */
+	public function __construct() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function edit() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function display() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function __destruct() {
+		// Not yet implemented
+	}
+}
+?>
---
+++ web/src/COrder.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @access public
+ * @author £uki
+ */
+class COrder {
+	private $m_ID;
+	private $m_IDUzytkownika;
+	private $m_ListaProduktow;
+	private $m_Status;
+
+	/**
+	 * @access public
+	 */
+	public function __construct() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function changeStatus() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function __destruct() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function displayStatus() {
+		// Not yet implemented
+	}
+
+	/**
+	 * @access public
+	 */
+	public function displayOrder() {
+		// Not yet implemented
+	}
+}
+?>
